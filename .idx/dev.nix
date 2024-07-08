c8758efbc6e55e28dd1ceeaed55ee8b9bc5f5515
balg.lg55 <balg.lg55@gmail.com>
2024-07-08 03:30:35 +0000

    first commit, add basic structure

---
+++ .idx/dev.nix
@@ -0,0 +1,45 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{pkgs}: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.yarn
+    pkgs.nodePackages.pnpm
+    pkgs.bun
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "esbenp.prettier-vscode"
+      "aaron-bond.better-comments"
+      "dracula-theme.theme-dracula"
+      "formulahendry.auto-close-tag"
+      "formulahendry.auto-rename-tag"
+      "yzhang.markdown-all-in-one"
+      "naumovs.color-highlight"
+      "bradlc.vscode-tailwindcss"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
+      };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="text-green-500">Hola mundo </h1>
+      <Button>Click me</Button>
+      <Button variant={"secondary"}>Click me</Button>
+      <Button>Click me</Button>
+    </div>
+  );
+}
