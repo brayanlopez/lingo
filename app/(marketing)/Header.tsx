653f35169036862e0a52543a14f0d565dd488d48
balg.lg55 <balg.lg55@gmail.com>
2024-07-08 03:57:26 +0000

    ADD env file and auth structure

--- app/(marketing)/Header.tsx
+++ app/(marketing)/Header.tsx
@@ -1,10 +1,46 @@
+import Image from "next/image";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
 export const Header = () => {
   return (
-    <header
-      className="h-20 w-full border-b-2
-     border-slate-200 px-4"
-    >
-      Header
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className="lg:max-w-screen mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image
+            src={"./images/mascot.svg"}
+            alt={"logo"}
+            width={40}
+            height={40}
+          />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button size={"lg"} variant={"ghost"}>
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
     </header>
   );
 };
---
+++ middleware.ts
@@ -0,0 +1,1 @@
+// TODO: put config for clerk
