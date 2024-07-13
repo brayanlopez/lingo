6846e76d73a24890cbb3d0215f82d63829ed3f91
Brayan Lopez <balg.lg55@gmail.com>
2024-07-12 19:29:49 -0500

    ADD courses page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,20 @@
-import FeedWrapper from "@/components/FeedWrapper";
-import StickyWrapper from "@/components/StickyWrapper";
-import Header from "./Header";
-import UserProgress from "@/components/UserProgress";
+import { redirect } from "next/navigation";
 
-const LearnPage = () => {
+import StickyWrapper from "@/components/StickyWrapper";
+import FeedWrapper from "@/components/FeedWrapper";
+import UserProgress from "@/components/UserProgress";
+import Header from "./Header";
+
+import { getUserProgress } from "@/db/queries";
+
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
   return (
     // TODO: remove flex-row-reverse
     <div className="flex flex-row-reverse gap-[48px] px-6">
